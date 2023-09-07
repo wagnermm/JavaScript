22b9304910c3ffc719d700272fdd5f19096470af
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-09-07 11:38:52 -0300

    validacao de email

--- src/newsletter-sign-up-with-success-message-main/js/script.js
+++ src/newsletter-sign-up-with-success-message-main/js/script.js
@@ -1,15 +1,32 @@
+let emailValido = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
 
-let form = document.getElementById('form');
-let email = document.getElementById('email').value;
-let erro = document.getElementById('erro');
-let btn = document.getElementById('button');
+const validEmail = (elemento) => {   
+    elemento.addEventListener('focusout', function(e) { 
+        e.preventDefault();    
+        if(this.value.match(emailValido)) {
+            document.querySelector('.info-erro').innerHTML = "";
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+            return false
+        } else {
+            document.querySelector('.info-erro').innerHTML = "valid email required";
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+        }
+    });
+};
 
-btn.addEventListener('click',(event)=> {
-    event.preventDefault();
-    //const validEmail =  /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?/i;
-    if(email == 1) {
-        erro.innerHTML = "parabens";
+let emailField = document.querySelectorAll('input#email');
+for( let emFoco of emailField) {
+    validEmail(emFoco);
+};
+
+const buttonAbled = ()=> {
+    const email = document.getElementById('email').value;
+    if(email.match(emailValido)) {
+        document.querySelector('#button').disabled = false;
+        return
     } else {
-        erro.innerHTML = "Valid email required";
-    }
-});
+        document.querySelector('#button').disabled = true;
+    };
+};
