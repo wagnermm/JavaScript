3bea5368ecd92bc5d5e75e6c9198408067c9f9d4
Wagner Miranda <miranda.wagnermarques@gmail.com>
2023-10-31 11:30:03 -0300

    criando paginacao com react-router-dom

--- src/crud-react/src/App.js
+++ src/crud-react/src/App.js
@@ -1,11 +1,29 @@
-
+import * as React from 'react';
+import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
 import Cadastro from './components/cadastro/Cadastro';
+import Home from './components/Home/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Cadastro/>
+      <BrowserRouter>
+        <header className='header'>
+          <div className='logo'>
+            <img alt='logo'/>
+          </div>
+          <nav className='nav'>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/cadastro">Cadastro</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/cadastro' element={<Cadastro/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
